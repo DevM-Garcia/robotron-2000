6f674dbfc4726f26ca9cb8fc6390c932b75315a2
Matheus Garcia <115594762+DevM-Garcia@users.noreply.github.com>
2023-07-02 15:12:02 -0300

    Reaproveitando o código
    Nesta aula eu extrai a parte do código que incrementava e a parte que subtraia um elemento no momento do click e os transferi para uma função, onde usei a estrutura de repetição if/else.
    
    Assim, a função fica responsável por realizar toda a operação enquanto o evento do click faz  a chamada para que a função seja executada.
    
    agora para que esse código fique mais dinâmico e que todas as peças interajam com o click,  usaremos o querySelectorAll.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,20 @@ const somar = document.querySelector('#somar')
 const subtrair = document.querySelector('#subtrair')
 const braco = document.querySelector('#braco')
 
-somar.addEventListener('click', (evento) =>{
-    braco.value = parseInt(braco.value) + 1 
+const controle = document.querySelectorAll(".controle-ajuste")
+console.log(controle)
+
+somar.addEventListener('click', () =>{
+    manipulaDados(somar)
 })
-subtrair.addEventListener('click', (evento)=>{
-    braco.value = parseInt(braco.value) - 1
+subtrair.addEventListener('click', ()=>{
+    manipulaDados(subtrair)
 })
 
+function manipulaDados (operacao) {
+    if (operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    }else{
+        braco.value = parseInt(braco.value) + 1
+    }
+}
