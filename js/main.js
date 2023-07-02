3d7bc7794d0c6599e1a8c2a30094ae10a0fb640a
Matheus Garcia <115594762+DevM-Garcia@users.noreply.github.com>
2023-07-02 16:13:56 -0300

    Alterando vários componentes
    Nesta aula aprendi a usar o método forEach,  que sem inventar muito ele realiza o laço de repetição de um array e nos retorna um índice e um elemento do Array.
    
    após declarar uma constante que nos retorna todos os elementos. acrescento o forEach solicitando dentro de seu parâmetro que o retorno seja de seu elemento, logo após acrscentamos o elemento com um escutador para que aguarde um click  e a partir do click ele execute outra função de seta chamando a função manipulaDados.
    
    Ao chamarmos a função manipulaDados precisamos manipular o evento que acontece naquele momento, então passamos o evento como parâmetro e chamos novamente na execução da chamada de função, só que agora com o .target que me retorna exatamente aonde foi clicado e também o .textContent que retorna o elemento de texto (diferente do .value que retorna o valor numérico) da string.
    
    Agora no corpo da função alterei o "subtrair" pelo "-" pois a função manipulaDados esta me retornando um texto por causa do textContent.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
-const somar = document.querySelector('#somar')
-const subtrair = document.querySelector('#subtrair')
 const braco = document.querySelector('#braco')
 
 const controle = document.querySelectorAll(".controle-ajuste")
-console.log(controle)
 
-somar.addEventListener('click', () =>{
-    manipulaDados(somar)
-})
-subtrair.addEventListener('click', ()=>{
-    manipulaDados(subtrair)
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) =>{
+        manipulaDados(evento.target.textContent)
+    })
 })
 
 function manipulaDados (operacao) {
-    if (operacao === "subtrair"){
+    if (operacao === "-"){
         braco.value = parseInt(braco.value) - 1
     }else{
         braco.value = parseInt(braco.value) + 1
